52280d95be5ef939ceef931bf596cb7468c2adc3
sahilvish01 <sahilvish6542@gmail.com>
2025-11-25 16:11:31 +0530

    Joseph's Problem

---
+++ Singly_ll/c20.cpp
@@ -0,0 +1,74 @@
+// Joseph's Problem
+
+#include<stdio.h>
+#include<iostream>
+#define null 0
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+node *first,*temp,*ttemp;
+
+void init()
+{
+   first = temp = ttemp = null; 
+}
+
+void firstnode(int x)
+{
+    first = new node;
+
+    first -> data = x;
+    first -> next = null;
+}
+
+void addnode(int x)
+{
+    temp = first;
+
+    while(temp -> next != null)
+    {
+        temp = temp -> next;
+    }
+
+    ttemp = new node;
+
+    temp -> next = ttemp;
+
+    ttemp -> data = x;
+    ttemp -> next = null;
+}
+
+
+
+void display()
+{
+    temp = first;
+
+    while(temp!= null)
+    {
+
+        cout << " " << temp -> data;
+        temp = temp -> next;
+    }
+}
+
+int main()
+{
+
+    init();
+
+    firstnode(10);
+
+    addnode(20);
+    addnode(30);
+    addnode(40);
+    addnode(50);
+
+    display();
+
+}
