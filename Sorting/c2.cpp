088d2fbadb1c89b116bc64b0a14f143e24430e7c
sahilvish01 <sahilvish6542@gmail.com>
2025-12-30 00:08:34 +0530

    Selection sort

---
+++ Sorting/c2.cpp
@@ -0,0 +1,41 @@
+// Selection sort :
+
+#include<iostream>
+using namespace std;
+
+int ssort(int arr[], int size)
+{
+    for(int i = size -1; i >= 0; i --)
+    {
+        int index = 0;
+        int large = arr[index];
+        for(int j = 1; j <= i; j++)
+        {
+            if(arr[index] < arr[j])
+            {
+                index = j;
+                large = arr[index];
+            }
+
+                int t = arr[index];
+                arr[index] = arr[i];
+                arr[i] = t;
+            
+        }
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+}
+
+int main()
+{
+    int arr[5]={3,7,1,3,5};
+
+    int size = sizeof(arr) / sizeof(int);
+
+    ssort(arr,size);
+}
