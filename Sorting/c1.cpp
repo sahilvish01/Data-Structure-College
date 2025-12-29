b02286cd5d964927dbb04618ef7fa25d0ff3b1f5
sahilvish01 <sahilvish6542@gmail.com>
2025-12-29 23:04:37 +0530

    BUbble sort

--- Sorting/c1.cpp
+++ Sorting/c1.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 int bsort(int arr[],int size)
 {
-    for(int i = size - 1; i >= 0; i--)
+    
+    for(int i = size - 1,check = 1; i >= 0 && check == 1; i--)
     {
+        check = 0;
+        cout << "Pass : " << size-i <<endl;
+
         for(int j = 0; j < i; j++)
         {
             if(arr[j] > arr[j+1])
             {
+                check = 1;
                 int t = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = t;
@@ -26,7 +31,9 @@ int bsort(int arr[],int size)
 
 int main()
 {
-    int arr[10] = {100,29,17,15,12,10,9,7,4,1};
+    // int arr[10] = {100,29,17,15,12,10,9,7,4,1};
+
+    int arr[6] = {1,2,3,4,5,6};
 
     int size = sizeof(arr)/sizeof(int);
 
